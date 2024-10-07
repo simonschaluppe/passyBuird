537e2444a3749137741fb66b6a2a5dc6cfda98f5
simonschaluppe <simon.schneider@technikum-wien.at>
2024-10-07 16:49:05 +0200

    font updates

--- font.py
+++ font.py
@@ -12,11 +12,19 @@ def clip(surf,x,y,x_size,y_size):
 class Font():
     def __init__(self, path):
         self.spacing = 1
-        self.character_order = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.','-',',',':','+','\'','!','?','0','1','2','3','4','5','6','7','8','9','(',')','/','_','=','\\','[',']','*','"','<','>',';','°']
+        self.character_order = ['A','B','C','D','E','F','G','H','I','J','K','L',
+                                'M','N','O','P','Q','R','S','T','U','V','W','X',
+                                'Y','Z','a','b','c','d','e','f','g','h','i','j',
+                                'k','l','m','n','o','p','q','r','s','t','u','v',
+                                'w','x','y','z','.','-',',',':','+','\'','!','?',
+                                '0','1','2','3','4','5','6','7','8','9','(',')',
+                                '/','_','=','\\','[',']','*','"','<','>',';','°',
+                                '#','²','³','|','??','%']
         font_img = pygame.image.load(path).convert()
         self.height = font_img.get_height()
         current_char_width = 0
         self.characters = {}
+        self.missing_char_replacement = "??"
         character_count = 0
         for x in range(font_img.get_width()):
             c = font_img.get_at((x, 0))
@@ -29,18 +37,30 @@ class Font():
                 current_char_width += 1
         self.space_width = self.characters['A'].get_width()
 
+    def character(self, char):
+        if char not in self.characters:
+            return self.characters[self.missing_char_replacement]
+        else: 
+            return self.characters[char]
+
     def surface(self, text, size=None, color=(10,10,10)):
         if not size: size = self.height
         scale = int(size/self.height)
-        small_surf = pygame.Surface((800,self.height))
-        x_offset = 0
+        xo, x_offsets = 0, [0]
         for char in text:
-            if char != ' ':
-                small_surf.blit(self.characters[char], (x_offset, 0))
-                x_offset += self.characters[char].get_width() + self.spacing
+            if char != " ":
+                xo += self.character(char).get_width() + self.spacing
             else:
-                x_offset += self.space_width + self.spacing
-        text = pygame.transform.scale(small_surf, (800*scale, self.height*scale))
+                xo += self.space_width + self.spacing  
+            x_offsets.append(xo)
+
+        width = x_offsets[-1]
+        small_surf = pygame.Surface((width,self.height))
+        for x_offset, char in zip(x_offsets,text):
+            if char != ' ':
+                small_surf.blit(self.character(char), (x_offset, 0))
+                
+        text = pygame.transform.scale(small_surf, (width*scale, self.height*scale))
         surf= pygame.Surface(text.get_size())
         surf.fill(color)
         text.set_colorkey((255,0,0))
@@ -74,13 +94,13 @@ if __name__ == '__main__':
     while True:
         
         # Background --------------------------------------------- #
-        screen.fill((0,232,0))
+        screen.fill((200,220,255))
 
 
         my_font.render(screen, 'Hello World!', (20, 20))
-        my_big_font.render(screen, 'Hello World! I am DaFluffyPotato.', (20, 40))
-        my_big_font.render(screen, 'Hello World! I am Simono.', (20, 60), 200)
-        my_font.render(screen, 'Hello World! I have °C.', (20, 120), 20)
+        my_big_font.render(screen, 'Press Escape to close', (20, 40))
+        my_big_font.render(screen, 'Hello World! I am Simono.', (20, 120), 100)
+        my_font.render(screen, 'Hello World! I have °C. and crazy "§$%³²', (20, 80), 20, color=(240,10,50))
         
         # Buttons ------------------------------------------------ #
         for event in pygame.event.get():
