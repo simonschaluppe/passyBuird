2accaf343faf299201c7f50404be8be1e127dbe0
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2026-03-13 13:09:57 +0100

    Display current level name in shop

--- model/GameModel.py
+++ model/GameModel.py
@@ -331,7 +331,7 @@ Electricity Price Discount: Lvl {self.upgrades['electricity_price_discount'].lev
 
     def get_game_stats(self):
         return {"lines": f"""
-            Current Level    {'dummy'}
+            Current Level    {self.current_level.name}
             Available Money  €{self.money}
             Average Comfort  {'dummy'}%
         """
