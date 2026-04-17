ecb94dbd46ea21536429da1cd7622bebac6dd720
simon.schneider@technikum-wien.at <simon.schneider@technikum-wien.at>
2026-04-17 15:32:04 +0200

    pv curve update

--- model/GameModel.py
+++ model/GameModel.py
@@ -210,10 +210,8 @@ class GameModel:
             "Date",
             points=[(h, ts) for h, ts in zip(range(8760), self.model.timestamp)],
         )
-        self.curve_pv = Curve(
-            "PV",
-            points=[(h, ts) for h, ts in zip(range(8760), self.model.PV.TSD)],
-        )
+        
+        self.setup_curve_pv()
 
         self.default_curve = Curve(
             "Default",
@@ -222,6 +220,12 @@ class GameModel:
         self.cleanup()
 
         return True
+    
+    def setup_curve_pv(self):
+        self.curve_pv = Curve(
+                "PV",
+                points=[(h, ts) for h, ts in zip(range(8760), self.model.PV.TSD)],
+            )
 
     def update_level_finished(self):
         """at the end of level, update comfort rating"""
@@ -581,6 +585,8 @@ class GameModel:
         def add_pv():
             kWp = self.model.PV.kWp + 5
             self.model.PV.set_kWp(kWp)
+            self.setup_curve_pv()
+            # pv 
 
         self.upgrades["wall_insulation"].callback = lambda: upgrade(
             self.upgrades["wall_insulation"], wall_insulation
